40e40d74dde5751d393d20f7f748baf809b5dbdf
LapshinIV <ivan.lapshin@39parrot.net>
2016-04-20 12:32:01 +0400

    test

---
+++ db.js
@@ -0,0 +1,82 @@
+var listOfFlights = 
+[
+  {
+    "flightNumber": "AB1322", 
+    "passengers": [ 
+        {
+          "name":     "Dmitry Kouznetsov",
+          "booking":  "ABCDEF"
+        },
+        {
+          "name":     "Natalia Shteinikova", 
+          "booking":  "ABCDEF"
+        }, 
+    ] 
+  }, 
+  { 
+    "flightNumber": "CD224",
+    "passengers": [ 
+        {
+          "name":     "Alex Emshanov",
+          "booking":  "ADADADAD"
+        },
+        {
+          "name":     "Alexandra Emshanova",
+          "booking":  "ADADADAD"
+        },
+       {
+          "name":     "Avrora Emshanova",
+          "booking":  "ADADADAD"
+        },
+    ] 
+  }, 
+  { 
+    "flightNumber": "QR335", 
+    "passengers": [ 
+        {
+          "name":     "Ivan Lapshin",
+          "booking":  "QWERT"
+        },
+        {
+          "name":     "Macaw Macaw",
+          "booking":  "QWERT"
+        },
+        {
+          "name": "Macaw1 Macaw1",
+          "booking":  "QWERT"
+        },
+    ] 
+  } 
+]
+
+
+function parsingFlights(){
+  for (var i=0; i<listOfFlights.length; i++){
+    if(listOfFlights[i].flightNumber){
+      document.write("<tr>",'<td class="choice">')
+      document.write(listOfFlights[i].flightNumber)
+      document.write("</td>","</tr>")
+    }
+  }
+}
+function parsingPassengers(){
+  for (var i=0; i<listOfFlights.length; i++){
+    for(var j = 0; j<listOfFlights[i].passengers.length; j++){
+      if(listOfFlights[i].passengers[j].name && listOfFlights[i].passengers[j].booking){
+          document.write("<tr>","<td>")
+          document.write(listOfFlights[i].passengers[j].name)
+          document.write("</td>","<td>")
+          document.write(listOfFlights[i].passengers[j].booking)
+          document.write("</td>","</tr>")
+      }
+    }
+  }
+}        
+
+
+
+
+
+
+
+
